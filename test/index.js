c7535690f6bb8322fb71875063e23f0612e6c853
Bruno Barreto <bruno@brunobarreto.net>
2018-01-31 11:44:48 -0200

    Testes agora verificam todas as informações.

--- test/index.js
+++ test/index.js
@@ -4,9 +4,28 @@ var should = require('chai').should(),
 
 
 describe('#consultaPlaca', function() {
-  it('consulta a placa AAA0001', function() {
+  it('consulta a placa AAA0001', function(done) {
     consultaPlaca('AAA0001', function (retorno) {
     	retorno.should.be.an('object');
+      done();
+    });
+  });
+});
+
+describe('#retornaErro', function() {
+  it('verifica se retornou código de erro 0', function(done) {
+    consultaPlaca('AAA0001', function (retorno) {
+    	retorno.should.have.a.property('codigoRetorno').equal('0');
+      done();
+    });
+  });
+});
+
+describe('#retornaErro', function() {
+  it('verifica se retornou o código da situação', function(done) {
+    consultaPlaca('AAA0001', function (retorno) {
+    	retorno.should.have.a.property('codigoSituacao');
+      done();
     });
   });
 });
